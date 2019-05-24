299611a1a81ba0a51e50faf8d91f92b8985a0aa4
milkway <leite@de.ufpe.br>
2019-05-24 13:51:40 -0300

    First commit

---
+++ inst/extdata/RandomCodes.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+
+distances <- c(0, 1, 1, 1, 1, 4, 2, 1,
+               1, 0, 3, 3, 3, 2, 1, 1, 
+               1, 3, 0, 4, 2, 2, 1, 2,
+               1, 3, 4, 0, 3, 3, 2, 1,
+               1, 3, 2, 3, 0, 1, 2, 3,
+               4, 2, 2, 3, 1, 0, 3, 1,
+               2, 1, 1, 2, 2, 3, 0, 1,
+               1, 1, 2, 1, 3, 1, 1, 0) %>% matrix(nrow = 8, byrow = TRUE)
+
+colnames(distances) <- 1:8
+rownames(distances) <- 1:8
+
+distances <- read_rds(paste0(system.file("extdata", package = "brkga"), "/MDG.1.a.n500m50.rds"))
+
+rst <- binarymodel(distances, m = 50, MAX_TIME = 600, THREADS = 8,  verbose = TRUE)
+
+N <- 500
+distances.d <- as.dist(distances[1:N,1:N])
+
+# plot(cluster.mdp)
+plot(cluster.mdp, hang = -1)
+rect.hclust(cluster.mdp, 50)
+table(cutree(cluster.mdp, 50))
+
+
+cluster.mdp <- hclust(distances.d)
+tour <- 
+  tibble(
+    N = 1:500,
+    Cluster = cutree(cluster.mdp, 200) 
+    ) %>% 
+  group_by(Cluster) %>% 
+  sample_n(1) %>% ungroup() %>% select(N) %>% unlist(use.names = FALSE) %>% 
+  sample(50)
+sum(distances[tour,tour])/2
---
+++ src/binarymodel.cpp
@@ -0,0 +1,139 @@
+#include "RcppArmadillo.h"
+// [[Rcpp::depends(RcppArmadillo)]]
+
+#include "gurobi_c++.h"
+#include <sstream>
+#include <chrono>
+
+// [[Rcpp::export]]
+Rcpp::List binarymodel(const arma::mat & distances, int m, double MAX_TIME = 10, int THREADS = 4, bool verbose = false) 
+    {
+    auto start = std::chrono::system_clock::now();
+    int n = distances.n_cols; 
+    if (n != distances.n_rows) // Need a Square Matrix
+        Rcpp::stop("Need a square matrix.");
+    if (m >= n) // Tour size senseless 
+        Rcpp::stop("Must have m < n.");
+    
+
+    arma::vec binaryTour(n, arma::fill::zeros);
+    Rcpp::IntegerVector tour;
+    int optimstatus = 0;
+    double fitness = 0;
+    
+    Rcpp::Rcout << "Creating Otimization Model..." << std::endl;
+    GRBEnv* env = NULL;
+    GRBVar* vars = NULL;
+    GRBVar** prod = NULL;
+    
+    try {
+        env = new GRBEnv();
+        GRBModel model = GRBModel(*env);
+        vars = new GRBVar[n];
+        
+        model.set(GRB_StringAttr_ModelName, "mdp");
+        model.getEnv().set(GRB_IntParam_OutputFlag, verbose);
+        model.getEnv().set(GRB_IntParam_Threads, THREADS);
+        model.set(GRB_IntAttr_ModelSense, GRB_MAXIMIZE);
+        
+        // Create binary decision variables
+        vars = model.addVars(n);
+        for (int i = 0; i < n; ++i) 
+        {
+            std::ostringstream vname;
+            vname << "V_" << i;
+            vars[i].set(GRB_CharAttr_VType, GRB_BINARY);
+            vars[i].set(GRB_StringAttr_VarName, vname.str());
+        }
+        
+        prod = new GRBVar*[n];
+        for (int i = 0; i < n; ++i) {
+            prod[i] = model.addVars(n);
+            for (int j = i + 1; j < n; ++j) {
+                std::ostringstream vname;
+                vname << "prod_" << i << "x" << j;
+                prod[i][j].set(GRB_CharAttr_VType, GRB_BINARY);
+                prod[i][j].set(GRB_StringAttr_VarName, vname.str());
+            }
+        }
+        
+        // Constraints (m elements in tour)
+        GRBLinExpr expr = 0;
+        for (int i = 0; i < n; i++) 
+            expr += vars[i];
+        model.addConstr(expr <= m, "TourSize");
+        
+        // Constraints (Linearization)
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i+1; j < n; j++) 
+            {
+                GRBLinExpr expr = prod[i][j];
+                std::ostringstream vname;
+                std::ostringstream vname1;
+                std::ostringstream vname2;
+                vname << "lin_" << i << "-" << j;
+                model.addConstr(expr <= vars[j], vname.str());
+                vname1 << "lin1_" << i << "-" << j;
+                model.addConstr(expr <= vars[i], vname1.str());
+                vname2 << "lin2_" << i << "-" << j;
+                model.addConstr(expr >= vars[i]+vars[j]-1, vname2.str());
+            }
+        }
+
+        // Forbid edge from node back to itself
+        for (int i = 0; i < n; i++)
+            prod[i][i].set(GRB_DoubleAttr_UB, 0);
+        
+        
+        GRBLinExpr obj = 0; //Total inner tour distance;
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                obj += prod[i][j]*distances(i,j);
+            }
+        } 
+    
+        model.getEnv().set(GRB_DoubleParam_TimeLimit, MAX_TIME);
+        model.setObjective(obj);
+        
+        // Write model to file
+        model.write("pdm.lp");
+        
+        // Optimize model
+        model.optimize();
+        
+        optimstatus = model.get(GRB_IntAttr_Status);
+        if (optimstatus == GRB_OPTIMAL || optimstatus == GRB_TIME_LIMIT) {
+            fitness = model.get(GRB_DoubleAttr_ObjVal);
+            Rcpp::Rcout << "Optimal objective: " << fitness << std::endl;
+            // Retrieve var[n] as IntegerVector
+            for(int i = 0; i < n; i++){
+                double val = vars[i].get(GRB_DoubleAttr_X);
+                binaryTour(i) = val;
+                if (val > 0.5)
+                    tour.push_back(i+1);
+            }
+        }
+    } catch (GRBException e) {
+        Rcpp::Rcout << "Error number: " << e.getErrorCode() << std::endl;
+        Rcpp::Rcout << e.getMessage() << std::endl;
+    } catch (...) {
+        Rcpp::Rcout << "Error during optimization" << std::endl;
+    }
+    
+    for (int i = 0; i < n; i++)
+        delete[] prod[i];
+    delete[] prod;
+    delete[] vars;
+    delete env;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    return Rcpp::List::create(Rcpp::Named("Tour")    = tour,
+                              Rcpp::Named("Binary")  = binaryTour,
+                              Rcpp::Named("Fitness") = fitness,
+                              Rcpp::Named("Duration")= elapsed_seconds.count(),
+                              Rcpp::Named("Status")  = optimstatus      
+                                  );
+    }
