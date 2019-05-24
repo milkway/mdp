80bf50e5e24ad3df4488aa60757ffd31b9a30744
milkway <leite@de.ufpe.br>
2019-05-24 16:13:48 -0300

    minor updates

--- inst/extdata/RandomCodes.R
+++ inst/extdata/RandomCodes.R
@@ -17,19 +17,20 @@ distances <- read_rds(paste0(system.file("extdata", package = "brkga"), "/MDG.1.
 rst <- binarymodel(distances, m = 50, MAX_TIME = 600, THREADS = 8,  verbose = TRUE)
 
 N <- 500
-distances.d <- as.dist(distances[1:N,1:N])
 
-# plot(cluster.mdp)
+distances <- read_rds(paste0(system.file("extdata", package = "brkga"), "/MDG.1.a.n500m50.rds"))
+distances.d <- as.dist(distances)
+cluster.mdp <- hclust(distances.d, method = "ward.D2")
 plot(cluster.mdp, hang = -1)
-rect.hclust(cluster.mdp, 50)
-table(cutree(cluster.mdp, 50))
+rect.hclust(cluster.mdp,50)
+
 
 
 cluster.mdp <- hclust(distances.d)
 tour <- 
   tibble(
     N = 1:500,
-    Cluster = cutree(cluster.mdp, 200) 
+    Cluster = cutree(cluster.mdp, 50) 
     ) %>% 
   group_by(Cluster) %>% 
   sample_n(1) %>% ungroup() %>% select(N) %>% unlist(use.names = FALSE) %>% 
